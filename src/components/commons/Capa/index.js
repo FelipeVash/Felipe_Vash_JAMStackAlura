50af977ec236e48a706e0906da40f54f45c68a47
FelipeVash <felipe.vash.dev@gmail.com>
2021-02-17 20:25:22 -0300

    Nova reorganização da arquitetura das pastas

--- src/components/commons/Capa/index.js
+++ src/components/commons/Capa/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import CapaWrapper from './styles/CapaWrapper';
 import LogoWrapper from './styles/LogoWrapper';
 
-const capaUrl = require("../../../../public/images/capa.jpg").default;
 const logoUrl = require("../../../../public/images/logo.jpeg").default;
+const capaUrl = require(`../../../../public/images/capaDark.jpg`).default;
 
 export default function Capa() {
   return (
