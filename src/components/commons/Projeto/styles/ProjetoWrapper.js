2d6c660b9b71ac70428976f2ac5abf9850cdf94f
FelipeVash <felipe.vash.dev@gmail.com>
2021-02-18 10:39:21 -0300

    Tentativa de correção na vercel

--- src/components/commons/Projeto/styles/ProjetoWrapper.js
+++ src/components/commons/Projeto/styles/ProjetoWrapper.js
@@ -16,6 +16,7 @@ const ProjetoWrapper = styled.div`
     width:20%;
     height: 90%;
     margin: 10px 20px 20px 20px;
+    padding: 10px;
     border-radius: 5%;
     border: 2px groove #4dffff;
     box-shadow: 1px 1px 10px 5px black;
@@ -59,7 +60,7 @@ const ProjetoImage = styled.img`
 const ProjetoNome = styled.p`
     font-size:1.5rem;
     font-weight:500;
-    color: #ff0066;
+    color: #7a0099;
     text-align: center;
     padding: 0px 10px 0px 10px;
     text-shadow: 10px 10px 10px 10px black;
