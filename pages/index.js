5587b3acd2e0dc8e5e9226140d6abe5cc165108f
FelipeVash <felipe.vash.dev@gmail.com>
2021-02-26 10:23:39 -0300

    Refeito useState para links do menu

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from 'next/link';
 import { Site } from '../src/components/commons/Site';
 import Capa from '../src/components/commons/Capa';
 import Menu from '../src/components/commons/Menu';
@@ -7,24 +6,28 @@ import Conteudo from '../src/components/commons/Projetos';
 import Footer from '../src/components/commons/Footer';
 
 export default function Home() {
+  const [conteudo, setConteudo] = React.useState('home');
   return (
     <Site>
-      <Capa />
-      <Menu>
-        {[
-          { url: '/', name: 'Home'},
-          { url: '/sobre', name: 'Sobre Mim'},
-          { url: 'https://www.linkedin.com/in/felipevash/', name: 'Linkedin'},
-        ].map((link) => (
-          <li key={link.name}>
-            <Link href={link.url}>
-              {link.name}
-            </Link>
-          </li>
-        ))}
-      </Menu>
-      <Conteudo />
-      <Footer />
+      <SiteWrapper>
+        <Capa />
+        <Menu>
+          {[
+            { name: 'Home', modo: 'home'},
+            { name: 'Sobre Mim', modo: 'sobre' },
+            { url: 'https://www.linkedin.com/in/felipevash/', name: 'Linkedin' },
+          ].map((link) => (
+            <li key={link.name}>
+              <Text variant="smallestException" tag="a" href={link.url} onClick={() => {setConteudo(link.modo)}}>
+                {link.name}
+              </Text>
+            </li>
+          ))}
+        </Menu>
+        {conteudo === 'home' && <Conteudo />}
+        {conteudo === 'sobre' && <Sobre />}
+        <Footer />
+      </SiteWrapper>
     </Site>
   )
 };
