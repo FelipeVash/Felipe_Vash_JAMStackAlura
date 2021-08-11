b8aadfff4dde76b4852c4825125308fd8dce6c72
Felipe Vash <felipe.silva@atonit.com.br>
2021-08-11 15:07:32 -0300

    feat: new background style

--- src/components/commons/Site/index.js
+++ src/components/commons/Site/index.js
@@ -6,7 +6,7 @@ import styled from 'styled-components';
 
 export const Site = styled.section`
   align-items: center;
-  background-color: ${({ theme }) => theme.colors.background.tertiary.color};
+  background-color: transparent;
   border-radius: 15px;
   display: flex;
   flex-direction: column;
